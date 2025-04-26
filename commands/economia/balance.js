a0b168ea503b45e3e6bdbda7ee464a556b590f6e
Guilherme <guiolivers15@gmail.com>
2025-04-26 01:42:15 -0300

    refactor: fix typo in import path for database utility
    The import path for the `getUser` function was corrected from `'../../utilidade/database'` to `'../../utilidades/database'` to match the actual directory structure. This ensures the module can be properly resolved and avoids runtime errors.

--- commands/economia/balance.js
+++ commands/economia/balance.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { getUser } = require('../../utilidade/database');
+const { getUser } = require('../../utilidades/database');
 
 module.exports = {
   data: new SlashCommandBuilder()
